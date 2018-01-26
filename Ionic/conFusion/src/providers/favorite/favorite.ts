2d07e5d732b6bbbe7c6aaa67f93040ba26bdf2c8
thien_000 <phuoc.huynh@expresspedia.net>
2018-01-26 10:09:12 +0700

    Ionic: FAB

---
+++ Ionic/conFusion/src/providers/favorite/favorite.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+
+/*
+  Generated class for the FavoriteProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class FavoriteProvider {
+  
+    favorites: Array<any>;
+  
+    constructor(public http: Http) {
+      console.log('Hello FavoriteProvider Provider');
+      this.favorites = [];
+    }
+  
+    addFavorite(id: number): boolean {
+      this.favorites.push(id);
+      return true;
+    }
+  
+    isFavorite(id: number): boolean {
+          return this.favorites.some(el => el === id);
+    }
+  }
