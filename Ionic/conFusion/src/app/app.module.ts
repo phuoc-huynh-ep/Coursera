e007b6572f3cee472f9048d31909fa2d05430db7
thien_000 <phuoc.huynh@expresspedia.net>
2018-01-29 09:18:44 +0700

    Ionic: Local Notifications

--- Ionic/conFusion/src/app/app.module.ts
+++ Ionic/conFusion/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HttpModule } from '@angular/http';
 import { IonicStorageModule } from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
--- Ionic/conFusion/src/providers/favorite/favorite.ts
+++ Ionic/conFusion/src/providers/favorite/favorite.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
 import { Storage } from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../dish/dish';
@@ -19,7 +20,8 @@ export class FavoriteProvider {
 
   constructor(public http: Http,
     private dishservice: DishProvider,
-    private storage: Storage) {
+    private storage: Storage,
+    private localNotifications: LocalNotifications) {
     console.log('Hello FavoriteProvider Provider');
     this.storage.get('favorites').then(favorites => {
       favorites ? this.favorites = favorites : this.favorites = [];
@@ -30,6 +32,11 @@ export class FavoriteProvider {
     if (!this.isFavorite(id)) {
       this.favorites.push(id);
       this.storage.set('favorites', this.favorites);
+      // Schedule a single notification
+      this.localNotifications.schedule({
+        id: id,
+        text: 'Dish ' + id + ' added as a favorite successfully'
+      });
     }
 
 
