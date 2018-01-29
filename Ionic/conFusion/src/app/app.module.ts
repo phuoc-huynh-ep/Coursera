6f51631353fb2b66e72bd170924731e79dc672db
thien_000 <phuoc.huynh@expresspedia.net>
2018-01-29 11:35:57 +0700

    Ionic: Network Status

--- Ionic/conFusion/src/app/app.module.ts
+++ Ionic/conFusion/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 import { EmailComposer } from '@ionic-native/email-composer';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { Camera } from '@ionic-native/camera'
+import { Network } from '@ionic-native/network';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -75,6 +76,7 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     EmailComposer,
     SocialSharing,
     Camera,
+    Network,
     { provide: 'BaseURL', useValue: baseURL },
     FavoriteProvider
   ]
