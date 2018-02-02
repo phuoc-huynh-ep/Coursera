5fab2db7b372444a7e5cb8b67c8027598c18ed2f
thien_000 <phuoc.huynh@expresspedia.net>
2018-02-03 00:12:46 +0700

    Node: OAuth 2

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '920843181415847',
+        clientSecret: '8baa33f648fe2bff2ab02caa37ee2c1d'
+    }
 }
